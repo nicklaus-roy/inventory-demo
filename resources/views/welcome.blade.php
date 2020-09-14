297e0523b76fffcfcd541e0c759b5df3e0bb8ab3
Sample <sampleemailonly854@gmail.com>
2020-09-13 23:06:41 -0700

    changed the welcome text again

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Hello Laravel
+                    Welcome to my site
                 </div>
 
                 <div class="links">
